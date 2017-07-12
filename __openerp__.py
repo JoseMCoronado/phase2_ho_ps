e92c9b9c50b11076958d5d6750fbcb7d13af260f
JoseMCoronado <j.coronado.personal@gmail.com>
2017-07-11 19:27:20 -0700

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Harbour July 2017 Phase 2 Customization',
+    'category': 'Sale',
+    'summary': 'Custom',
+    'version': '1.0',
+    'description': """
+A Variet of improvements to sales...
+        """,
+    'depends': ['base_action_rule','base_import','sale','purchase','stock','website_quote'],
+    'data': [
+        'data/ir_model.xml',
+        'data/ir_model_fields.xml',
+        'data/ir_actions_act_window.xml',
+        'data/ir_actions_server.xml',
+        'data/ir_ui_view.xml',
+        #'data/base_automation.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
