aa4f309ca2b52c85e82b2b1af59e8109ecf63e82
Jose M. Coronado <j.coronado.personal@gmail.com>
2017-08-16 19:48:13 -0700

    Last Edits before pushing into production.

--- __openerp__.py
+++ __openerp__.py
@@ -2,12 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Harbour July 2017 Phase 2 Customization',
+    'name': 'Harbour Q3 2017 Phase 2 Customization',
     'category': 'Sale',
     'summary': 'Custom',
     'version': '1.0',
     'description': """
-A Variet of improvements to sales...
+A variety of commissioned changes to Harbour's existing database. Ask JOS for github repo. 
         """,
     'depends': ['base_action_rule','base_import','sale','purchase','stock','website_quote','account'],
     'data': [
